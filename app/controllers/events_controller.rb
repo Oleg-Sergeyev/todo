f0fe9267cabf1c193a750505b09404991296c478
Oleg-Sergeyev <sergeyev_ov@mail.ru>
2022-01-06 23:50:25 +0300

    Added rows count for events view in task 9.3

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,13 @@
 # class EventsController
 class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
-
+  @rows_count = 5
   # GET /events or /events.json
   def index
-    if cookies[:start_date].nil? && cookies[:final_date].nil?
+    if cookies[:start_date].nil? && cookies[:final_date].nil? && cookies[:rows_count].nil?
       cookies.permanent[:start_date] = DateTime.now.beginning_of_day
       cookies.permanent[:final_date] = DateTime.now.end_of_day
+      cookies.permanent[:rows_count] = @rows_count
     end
     @start_date = cookies[:start_date].to_time
     @final_date = cookies[:final_date].to_time
@@ -29,7 +30,9 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    if event_params.key?('start_date') || event_params.key?('final_date')
+    if event_params.key?('start_date') || event_params.key?('final_date') || event_params.key?('rows_count')
+      @rows_count = event_params[:rows_count].to_i
+      cookies.permanent[:rows_count] = event_params[:rows_count].to_i
       check_dates([event_params[:start_date].to_time, event_params[:final_date].to_time])
     else
       @event = Event.new(event_params.merge(user: User.first))
@@ -77,11 +80,11 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.require(:event).permit(:name, :content, :done, :user, :start_date, :final_date, :page)
+    params.require(:event).permit(:name, :content, :done, :user, :start_date, :final_date, :rows_count)
   end
 
   def get_data(start_date = nil, final_date = nil)
-    Event.where(created_at: start_date...final_date).includes(:items).page(params[:page]).per(10)
+    Event.where(created_at: start_date...final_date).includes(:items).page(params[:page]).per(cookies[:rows_count])
   end
 
   def render_query
@@ -91,15 +94,18 @@ class EventsController < ApplicationController
 
   def check_dates(dates)
     if dates.first.nil? && dates.last.nil?
-      @events = Event.includes(:items).page(params[:page]).per(10)
-      @start_date = Event.where(created_at: Event.select('MIN(created_at)')).pluck(:created_at).first.to_time.beginning_of_day
-      @final_date = Event.where(created_at: Event.select('MAX(created_at)')).pluck(:created_at).first.to_time.end_of_day
+      @events = Event.includes(:items).page(params[:page]).per(cookies[:rows_count])
+      @start_date = Event.where(created_at: Event.select('MIN(created_at)'))
+                         .pluck(:created_at).first.to_time.beginning_of_day
+      @final_date = Event.where(created_at: Event.select('MAX(created_at)'))
+                         .pluck(:created_at).first.to_time.end_of_day
     elsif dates.first && dates.last
       @start_date = dates.first.beginning_of_day
       @final_date = dates.last.end_of_day
       @events = get_data(@start_date, @final_date)
     elsif dates.first.nil?
-      @start_date = Event.where(created_at: Event.select('MIN(created_at)')).pluck(:created_at).first.to_time.beginning_of_day
+      @start_date = Event.where(created_at: Event.select('MIN(created_at)'))
+                         .pluck(:created_at).first.to_time.beginning_of_day
       @final_date = dates.last.end_of_day
       @events = get_data(@start_date, @final_date)
     elsif dates.last.nil?
