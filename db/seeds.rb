06fcd9f1af282425aa90c7d1842243fb2379758c
Oleg-Sergeyev <sergeyev_ov@mail.ru>
2022-01-11 15:50:52 +0300

    Changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,75 +1,77 @@
 # frozen_string_literal: true
 
-# Comment.destroy_all
-# Seo.destroy_all
-# Item.destroy_all
-# Event.destroy_all
-# User.destroy_all
-# Role.destroy_all
+Comment.destroy_all
+Seo.destroy_all
+Item.destroy_all
+Event.destroy_all
+User.destroy_all
+Role.destroy_all
 
-# Role.create!(name: 'Пользователь', code: :default)
+Role.create!(name: 'Пользователь', code: :default)
 
-# hash_users = 10.times.map do
-#   {
-#     name: FFaker::Internet.user_name[0...15],
-#     email: FFaker::Internet.safe_email,
-#     role: Role.find_by(code: :default),
-#     active: [true, false].sample,
-#     events_unffd_count: 0,
-#     events_ffd_count: 0,
-#     items_unffd_count: 0,
-#     items_ffd_count: 0
-#   }
-# end
+hash_users = 10.times.map do
+  {
+    name: FFaker::Internet.user_name[0...15],
+    email: FFaker::Internet.safe_email,
+    password: 'password',
+    role: Role.find_by(code: :default),
+    active: [true, false].sample,
+    events_unffd_count: 0,
+    events_ffd_count: 0,
+    items_unffd_count: 0,
+    items_ffd_count: 0
+  }
+end
 
-# users = User.create! hash_users
+users = User.create! hash_users
 
-# hash_events = 20.times.map do
-#   {
-#     name: FFaker::HipsterIpsum.phrase,
-#     content: FFaker::HipsterIpsum.paragraphs,
-#     user: users.sample,
-#     done: [true, false].sample
-#   }
-# end
+hash_events = 20.times.map do
+  {
+    name: FFaker::HipsterIpsum.phrase,
+    content: FFaker::HipsterIpsum.paragraphs,
+    user: users.sample,
+    done: [true, false].sample
+   }
+end
 
-# events = Event.create! hash_events
-# hash_items = 200.times.map do
-#   {
-#     name: FFaker::HipsterIpsum.paragraph,
-#     event: events.sample,
-#     done: [true, false].sample
-#   }
-# end
-# Item.create! hash_items
+events = Event.create! hash_events
+hash_items = 200.times.map do
+  {
+    name: FFaker::HipsterIpsum.paragraph,
+    event: events.sample,
+    done: [true, false].sample,
+    #created_at: [ DateTime.now, DateTime.now - 2.day, DateTime.now - 3.day, DateTime.now - 4.day].sample
+  }
+end
+Item.create! hash_items
 
-# hash_comments = 200.times.map do
-#   commentable = (rand(2) == 1 ? events : users).sample
-#   {
-#     content: FFaker::Lorem.paragraph,
-#     user: users.sample,
-#     commentable_id: commentable.id,
-#     commentable_type: commentable.class.to_s
-#   }
-# end
+hash_comments = 200.times.map do
+  commentable = (rand(2) == 1 ? events : users).sample
+  {
+    content: FFaker::Lorem.paragraph,
+    user: users.sample,
+    commentable_id: commentable.id,
+    commentable_type: commentable.class.to_s
+  }
+end
 
-# comments = Comment.create! hash_comments
+comments = Comment.create! hash_comments
 
-# hash_seos = 200.times.map do
-#   promoted = (rand(2) == 1 ? comments : users).sample
-#   {
-#     # comment: comments.sample,
-#     title: FFaker::Lorem.sentence(1),
-#     description: FFaker::Lorem.sentence(10),
-#     keywords: FFaker::Lorem.sentence(12),
-#     promoted_id: promoted.id,
-#     promoted_type: promoted.class.to_s
-#   }
-# end
+hash_seos = 200.times.map do
+  promoted = (rand(2) == 1 ? comments : users).sample
+  {
+    # comment: comments.sample,
+    title: FFaker::Lorem.sentence(1),
+    description: FFaker::Lorem.sentence(10),
+    keywords: FFaker::Lorem.sentence(12),
+    promoted_id: promoted.id,
+    promoted_type: promoted.class.to_s
+  }
+end
 
-# Seo.create! hash_seos
+Seo.create! hash_seos
 
-# User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 hash_user =
   {
     name: 'admin',
