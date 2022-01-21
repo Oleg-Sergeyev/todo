1e6a3e6683ad89a6168c0ead5b682fd9968e33b1
Oleg-Sergeyev <sergeyev_ov@mail.ru>
2022-01-21 18:27:18 +0300

    Added manage for roles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   
   namespace :admin do
+    resources :roles
+  end
+  namespace :admin do
     root 'users#index'
     resources :users
   end
@@ -12,15 +15,19 @@ Rails.application.routes.draw do
   match 'users/:id' => 'admin/users#destroy', via: :delete, as: :admin_destroy_user
   match 'users/:id' => 'admin/users#create', via: :create, as: :admin_create_user
   resources :users
+
+  match 'roles/:id' => 'admin/roles#destroy', via: :delete, as: :admin_destroy_role
+  match 'roles/:id' => 'admin/roles#create', via: :create, as: :admin_create_role
+  resources :roles
   # devise_for :user, path_names: {
   #   sign_in: 'login', sign_out: 'logout'
   # }
   resources :events do
     resources :items
   end
-  #resources :users
+  resources :events
   # get 'events/page/(:page(.:format))', to: 'events#index'
-  get 'events/page', to: 'events#index'
+  #get 'events/page', to: 'events#index'
   get 'stats', to: 'stats#index'
   get 'about', to: 'about#index'
   # get 'calendar', to: 'calendar#index', as: 'calendar_index'
