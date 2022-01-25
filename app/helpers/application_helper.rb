c8cd1dec9a04f5dd23242f1ae1ac2dc35f61db0f
Oleg-Sergeyev <sergeyev_ov@mail.ru>
2022-01-25 19:27:51 +0300

    Added task 4.3 cout event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
   def language_link_name
     session[:locale] == I18n.default_locale.to_s ? 'EN' : 'RU'
   end
+
+  def count_title(klass, count)
+    count ||= 0
+    number = count.to_i.digits.reverse.last.to_i
+    count_name = 'many' if (5..9).include?(number) || number.zero?
+    count_name = 'others' if (2..4).include? number
+    count_name = 'one' if number == 1
+    "#{count} #{t("activerecord.attributes.#{klass.name.downcase}.#{count_name}")}"
+  end
 end
