a4a384258eb1a1af9a30323364ac2b361f4bf590
Oleg-Sergeyev <sergeyev_ov@mail.ru>
2022-01-27 03:56:22 +0300

    Add js enable submit log_in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,7 +36,6 @@ window.addEventListener("load", () => {
   active_links.forEach((element) => {
     element.addEventListener("ajax:success", (event) => {
       element.innerHTML = element.innerHTML == "Active" ? "Disactive" : "Active"
-      console.log(element.innerHTML)
     });
   });
 });
@@ -45,20 +44,37 @@ window.onload = function() {
   check_email_input()
 };
 
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   check_email_input();
+// });
+
+// Валидация электронной почты
 function ValidateEmail(mail) 
 {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if(mail.match(mailformat)) { return (true) };
   return (false);
 }
-
+// Проверка в поле введенного адресса эл.почты 
 function check_email_input()
 {
   let input = document.getElementById('input_email');
   let warning_label = document.getElementById('label_wrong_email');
   input.addEventListener('input', () => { 
-    if (input.value == '') { warning_label.innerHTML = ' '; return; }
-    if (ValidateEmail(input.value) == false) { warning_label.innerHTML = 'Wrong_email!'; }
-      else{ warning_label.innerHTML = ' '; }
+    if (input.value == '') { warning_label.innerHTML = ' '; toggle_submit('off'); return; }
+    if (ValidateEmail(input.value) == false) { 
+      warning_label.innerHTML = 'Wrong_email!';
+      toggle_submit('off'); 
+    }
+      else{ warning_label.innerHTML = ' '; 
+      toggle_submit('on'); 
+    }
   });
+}
+// Включение отключение кнопки подтверждения
+function toggle_submit(val){
+  let button_submit = document.getElementById("submit_log_in");
+  if (val == 'on') {
+    button_submit.disabled = false
+  } else { button_submit.disabled = true }
 }
